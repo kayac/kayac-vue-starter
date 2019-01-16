4192acbda29ab0cd0017caf6e8c028f0c1f0e03d
aykikr <nakakubo.ayaka@gmail.com>
2019-01-16 18:37:21 +0900

    テスト表示

--- src/components/RegisterForm.vue
+++ src/components/RegisterForm.vue
@@ -1,16 +1,19 @@
 <template lang="pug">
 form.form-items(@submit.prevent="submission")
   .form-item
-    label(for="name") 名前
+    label(for="name") {{ formItemName.name }}
     input(name="name" v-model.trim="formData.name")
+    p.form-warning(v-if="warningText.name") {{ warningText.name }}
 
   .form-item
-    label(for="mail") メールアドレス
+    label(for="mail")  {{ formItemName.mail }}
     input(name="mail" v-model.trim="formData.mail" type="email")
+    p.form-warning(v-if="warningText.mail") {{ warningText.mail }}
 
   .form-item
-    label(for="password") パスワード
+    label(for="password")  {{ formItemName.password }}
     input(name="password" v-model.trim="formData.password" type="password")
+    p.form-warning(v-if="warningText.password") {{ warningText.password }}
 
   .form-item
   button(type="submit") 送信
@@ -21,10 +24,20 @@ export default {
   name: "RegisterForm",
   data: () => {
     return {
+      formItemName: {
+        name: "名前",
+        mail: "メールアドレス",
+        password: "パスワード"
+      },
       formData: {
         name: "dely",
         mail: "email@email.jp",
         password: "password"
+      },
+      warningText: {
+        name: null,
+        mail: null,
+        password: null
       }
     };
   },
@@ -43,6 +56,7 @@ export default {
       Object.keys(this.formData).forEach((formDataItem) => {
         if( this.formData[formDataItem].length === 0) {
           result = false;
+          this.warningText[formDataItem] = this.formItemName[formDataItem] + "を入力してください"
         }
       });
       console.log(result);
@@ -55,8 +69,11 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 .form {
-  &-require {
-    font-weight: bold;
+  &-item {
+    padding-bottom: 20px;
+  }
+  &-warning {
+    font-size: 80%;
   }
 }
 </style>
