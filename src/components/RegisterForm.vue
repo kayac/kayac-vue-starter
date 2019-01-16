1eab32673a155b6ef25fed8c13fe631a49d79993
aykikr <nakakubo.ayaka@gmail.com>
2019-01-16 15:30:06 +0900

    送信用メソッド追加

--- src/components/RegisterForm.vue
+++ src/components/RegisterForm.vue
@@ -1,19 +1,19 @@
 <template lang="pug">
-form.form-items
+form.form-items(@submit.prevent="submission")
   .form-item
     label(for="name") 名前
-    input(name="name" v-model="name")
+    input(name="name" v-model="formData.names")
 
   .form-item
     label(for="mail") メールアドレス
-    input(name="mail" v-model="mail" type="email")
+    input(name="mail" v-model="formData.mail" type="email")
 
   .form-item
     label(for="password") パスワード
-    input(name="password" v-model.trim="password" type="password")
+    input(name="password" v-model.trim="formData.password" type="password")
 
   .form-item
-  button 送信
+  button(type="submit") 送信
 </template>
 
 <script>
@@ -21,10 +21,17 @@ export default {
   name: "RegisterForm",
   data: () => {
     return {
-      name: "f",
-      mail: "",
-      password: ""
+      formData: {
+        names: "f",
+        mail: "ee@mm.ll",
+        password: "pass"
+      }
     };
+  },
+  methods: {
+    submission: function (){
+      console.log(this.formData);
+    }
   }
 };
 </script>
