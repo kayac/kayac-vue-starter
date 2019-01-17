78769322018c45a93ae2facd6446970c1c1e2d69
aykikr <nakakubo.ayaka@gmail.com>
2019-01-17 16:44:59 +0900

    構造変更

--- src/components/RegisterForm.vue
+++ src/components/RegisterForm.vue
@@ -1,19 +1,21 @@
 <template lang="pug">
 .form-items
   .form-item
-    label(for="name") {{ formItemName.name }}
+    label(for="name") 名前
     input(name="name" v-model.trim="formData.name" type="text")
-    p.form-warning(v-if="warningText.name") {{ warningText.name }}
+    p.form-warning(v-if="warningState.isEmptyCheck.name") 名前を入力してください
+    p.form-warning(v-if="warningState.isnameLength") 名前は５文字以上にしてください
 
   .form-item
-    label(for="mail")  {{ formItemName.mail }}
+    label(for="mail") メール
     input(name="mail" v-model.trim="formData.mail" type="email")
-    p.form-warning(v-if="warningText.mail") {{ warningText.mail }}
+    p.form-warning(v-if="warningState.isEmptyCheck.name") メールを入力してください
+    p.form-warning(v-if="warningState.emptyMail") 正しいメールの形式にしてください
 
   .form-item
-    label(for="password")  {{ formItemName.password }}
+    label(for="password") パスワード
     input(name="password" v-model.trim="formData.password" type="password")
-    p.form-warning(v-if="warningText.password") {{ warningText.password }}
+    p.form-warning(v-if="warningState.isEmptyCheck.password") パスワードを入力してください
 
   .form-item
   button(@click="submission") 送信
@@ -24,28 +26,25 @@ export default {
   name: "RegisterForm",
   data: () => {
     return {
-      formItemName: {
-        name: "名前",
-        mail: "メールアドレス",
-        password: "パスワード"
-      },
       formData: {
         name: "名前５文字",
         mail: "email@email.jp",
         password: "password"
       },
-      warningText: {
-        name: null,
-        mail: null,
-        password: null
+      warningState: {
+        isEmptyCheck: {
+          name: false,
+          mail: false,
+          password: false,
+        },
+        isNameLengthCheck: false,
+        isMailTypeCheck: false
       }
     };
   },
   methods: {
     // 送信ボタンが押された時の処理
     submission: function() {
-      this.warningTextInitialize();
-      // 空白チェック
       if (this.isEmpty() && this.nameLengthCheck() && this.mailTypeCheck()){
         console.log("result: success");
       } else {
@@ -56,40 +55,24 @@ export default {
     isEmpty: function() {
       let result = true;
       Object.keys(this.formData).forEach((formDataItem) => {
-        if(this.formData[formDataItem].length === 0) {
-          result = false;
-          this.warningText[formDataItem] = this.formItemName[formDataItem] + "を入力してください";
-        }
+        result = (this.formData[formDataItem].length === 0) ? true : false;
+        this.warningState.isEmptyCheck[formDataItem] = !result;
       });
       return result;
     },
     nameLengthCheck: function() {
       // console.log("lengthCheck:" + (this.formData.name.length <= 4));
       // console.log("length:" + this.formData.name.length);
-      if (this.formData.name.length <= 4) {
-        this.warningText.name = "名前は５文字以上入力してください";
-        return false;
-      }
-      return true;
+      this.warningState.nameLengthCheck = (this.formData.name.length <= 4) ? true : false;
+      return !this.warningState.nameLengthCheck;
     },
     mailTypeCheck: function() {
       let type = new RegExp(/[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z0-9-]/);
       // console.log(this.formData.mail);
       // console.log(type);
       // console.log("mailTypeCheck: " + type.test(this.formData.mail));
-      if (!type.test(this.formData.mail)) {
-        this.warningText.mail = "正しいメールの形式を入力してください";
-        return false;
-      }
-      return true;
-    },
-    warningTextInitialize: function() {
-      // 中身を初期化
-      this.warningText = {
-        name: null,
-        mail: null,
-        password: null
-      };
+      this.warningState.isMailTypeCheck = type.test(this.formData.mail) ? true : false;
+      return !this.warningState.mailTypeCheck;
     }
   }
 };
